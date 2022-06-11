2ece2518a2a8d3af6b52519e4ac70218c75318e1
LucasM <lucassss8v@gmail.com>
2022-06-11 11:38:24 -0300

    Confirmation PopUp done

--- script.js
+++ script.js
@@ -5,26 +5,74 @@
      //   this.classList.add("border");
   //  });
 //}
+// Global Variables
+
+let selectedDish;
+let selectedBeverage;
+let selectedDessert;
+let dishes;
+let beverage;
+let dessert;
+let cancelAction;
+
+
+
+// Selection functions
+
 function dishSelection(element){
-  let selectedDish = document.querySelector(".dishes.border");
+  
+   dishes = element.innerHTML; 
+   selectedDish = document.querySelector(".dishes.border");
     if (selectedDish !== null ){
     selectedDish.classList.remove("border")
     }
-  element.classList.add("border")    
+  element.classList.add("border")
+  buttonLayout()
 }
 
 function beverageSelection(element){
-  let selectedBeverage = document.querySelector(".beverage.border");
+
+  beverage = element.innerHTML;
+  selectedBeverage = document.querySelector(".beverage.border");
     if (selectedBeverage !== null ){
     selectedBeverage.classList.remove("border")
     }
-  element.classList.add("border")    
+  element.classList.add("border")
+  buttonLayout()
+      
 }
 
 function dessertSelection(element){
-  let selectedDessert = document.querySelector(".desserts.border");
+  dessert = element.innerHTML;
+  selectedDessert = document.querySelector(".desserts.border");
     if (selectedDessert !== null ){
     selectedDessert.classList.remove("border")
     }
-  element.classList.add("border")    
+  element.classList.add("border")  
+  buttonLayout()
+     
 }
+
+// Hidden/PopUp function
+
+function checkout(){
+  
+  if (dishes && beverage && dessert){
+    let hidden = document.querySelector(".hidden1");
+    hidden.classList.remove("hidden1");
+   
+}
+}
+
+function buttonLayout(){
+  if( dishes && beverage && dessert){
+    let btn = document.querySelector(".checkout");
+    let visibleBtn = document.querySelector(".Visible_btn")
+    btn.classList.add("background")
+    btn.classList.remove("hidden")
+    visibleBtn.classList.add("hidden")
+  }
+}
+
+
+
